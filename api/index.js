ce9301e38e6713d7dfcb682abf0bf78b1ce3c579
piducancore:/# <hola@piducan.dev>
2021-12-13 01:52:11 -0300

    no trim

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const getColumns = async (spreadsheetId, range) => {
     const auth = await google.auth.getClient({
       credentials: {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n").trim(),
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
       },
       scopes: "https://www.googleapis.com/auth/spreadsheets.readonly",
     });
